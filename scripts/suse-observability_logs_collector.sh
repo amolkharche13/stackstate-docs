dac91950517596cc42d8d20db21899615cbb0630
Amol Kharche <amol.kharche@suse.com>
2024-12-05 17:57:05 +0530

    Added log messages with timestamps.

--- scripts/suse-observability_logs_collector.sh
+++ scripts/suse-observability_logs_collector.sh
@@ -4,25 +4,41 @@
 # Namespace to collect information
 NAMESPACE="suse-observability"
 
+# Check if namespace exist or not
+if ! kubectl get namespace "$NAMESPACE" &>/dev/null; then
+    echo "Namespace '$NAMESPACE' does not exist. Exiting."
+    exit 1
+fi
+
+# Check if kubectl is installed or not
+if ! command -v kubectl &>/dev/null; then
+   echo "kubectl is not installed. Please install it and try again."
+   exit 1
+fi
+
 # Check if KUBECONFIG is set
 if [[ -z "$KUBECONFIG" || ! -f "$KUBECONFIG" ]]; then
-  echo "Error: KUBECONFIG is not set"
-  echo "Please ensure KUBECONFIG is set to the path of a valid kubeconfig file before running this script. Exiting..."
-  exit 1
+    echo "Error: KUBECONFIG is not set. Please ensure KUBECONFIG is set to the path of a valid kubeconfig file before running this script."
+    echo "If kubeconfig is not set, use the command: export KUBECONFIG=PATH-TO-YOUR/kubeconfig. Exiting..."
+ exit 1
 fi
 # Directory to store logs
 OUTPUT_DIR="${NAMESPACE}_logs_$(date +%Y%m%d%H%M%S)"
 ARCHIVE_FILE="${OUTPUT_DIR}.tar.gz"
 mkdir -p "$OUTPUT_DIR"
 
+techo() {
+  echo "$(date '+%Y-%m-%d %H:%M:%S'): $*" | tee -a $OUTPUT_DIR/collector-output.log
+}
 
-echo "Collecting node details..."
+
+techo "Collecting node details..."
 kubectl get nodes -o wide > "$OUTPUT_DIR/nodes_status"
 kubectl describe nodes > "$OUTPUT_DIR/nodes_describe"
 
 # Function to collect yaml
 collect_yaml_configs() {
-    echo "Collecting YAML configurations..."
+    techo "Collecting YAML configurations..."
 
     mkdir -p "$OUTPUT_DIR/yaml"
 
@@ -41,12 +57,11 @@ collect_yaml_configs() {
     # Cronjob YAMLs
     kubectl -n "$NAMESPACE" get cronjob -o yaml > "$OUTPUT_DIR/yaml/cronjob.yaml"
 
-    echo "YAML configurations collected."
 }
 
 # Function to collect pod logs
 collect_pod_logs() {
-    echo "Collecting pod logs..."
+    techo "Collecting pod logs..."
     PODS=$(kubectl -n "$NAMESPACE" get pods -o jsonpath="{.items[*].metadata.name}")
     for pod in $PODS; do
         mkdir -p "$OUTPUT_DIR/pods/$pod"
@@ -56,37 +71,36 @@ collect_pod_logs() {
             kubectl -n "$NAMESPACE" logs "$pod" -c "$container" --previous > "$OUTPUT_DIR/pods/$pod/${container}_previous.log" 2>/dev/null
         done
     done
-    echo "Pod logs collected."
-}
+ }
 
 # Collect general pod statuses
-echo "Collecting pod statuses..."
+techo "Collecting pod statuses..."
 kubectl -n "$NAMESPACE" get pods -o wide > "$OUTPUT_DIR/pods_status"
 
 # Collect StatefulSets information
-echo "Collecting StatefulSets information..."
+techo "Collecting StatefulSets information..."
 kubectl -n "$NAMESPACE" get statefulsets -o wide > "$OUTPUT_DIR/statefulsets"
 kubectl -n "$NAMESPACE" describe statefulsets > "$OUTPUT_DIR/statefulsets_describe"
 
 # Collect DaemonSets information
-echo "Collecting DaemonSets information..."
+techo "Collecting DaemonSets information..."
 kubectl -n "$NAMESPACE" get daemonsets  > "$OUTPUT_DIR/daemonsets"
 kubectl -n "$NAMESPACE" describe daemonsets > "$OUTPUT_DIR/daemonsets_describe"
 
-echo "Collecting Deployments information..."
+techo "Collecting Deployments information..."
 kubectl -n "$NAMESPACE" get deployments -o wide > "$OUTPUT_DIR/deployments"
 
-echo "Collecting services information..."
+techo "Collecting services information..."
 kubectl -n "$NAMESPACE" get services -o wide > "$OUTPUT_DIR/services"
 
-echo "Collecting information about configmaps and secrets..."
+techo "Collecting information about configmaps and secrets..."
 kubectl -n "$NAMESPACE" get configmaps -o wide > "$OUTPUT_DIR/configmaps"
 kubectl -n "$NAMESPACE" get secrets -o wide > "$OUTPUT_DIR/secrets"
 
-echo "Collecting cronjob information..."
+techo "Collecting cronjob information..."
 kubectl -n "$NAMESPACE" get cronjob -o wide > "$OUTPUT_DIR/cronjob"
 
-echo "Collecting events in $NAMESPACE ..."
+techo "Collecting events in $NAMESPACE ..."
 kubectl -n "$NAMESPACE" get events --sort-by='.metadata.creationTimestamp' > "$OUTPUT_DIR/events"
 
 archive_and_cleanup() {
